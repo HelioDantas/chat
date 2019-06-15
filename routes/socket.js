8f930c6aab31828eaaeb140ff3e4e3cca7bc80a4
Helio Dantas <helio_dantas@outlook.com>
2019-06-15 03:28:14 -0300

    tt
    Signed-off-by: Helio Dantas <helio_dantas@outlook.com>

--- routes/socket.js
+++ routes/socket.js
@@ -3,8 +3,8 @@ const message = require('../model/message');
 module.exports = async (socket) => {
     const messages = await message.all();
     socket.emit('previossMessage', messages);
-    socket.on('sendMenssage', data => {
-        message.push(data);
+    socket.on('sendMenssage', async data => {
+        await message.push(data);
         socket.broadcast.emit('retornMensage', data);
     })
 };
