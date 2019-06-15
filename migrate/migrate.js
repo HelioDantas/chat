fab92cb2f19cc75a2fe47bce554d3c4b54668594
Helio Dantas <helio_dantas@outlook.com>
2019-06-15 03:01:59 -0300

    foidevez
    Signed-off-by: Helio Dantas <helio_dantas@outlook.com>

--- migrate/migrate.js
+++ migrate/migrate.js
@@ -4,13 +4,13 @@ const Banck = require('../datasouce/bank');
 const bank = new Banck();
 
 const main = async () => {
-    bank.isConnected();
+ /*   bank.isConnected();
     bank.read('create sequence message_id_seq;\n' +
         '\n' +
         'alter table message alter column id set default nextval(\'public.message_id_seq\');\n' +
         '\n' +
         'alter sequence message_id_seq owned by message.id;');
-
+*/
 };
 
 main();
