0900279073dcd734f8966df97364f3d17f8461ad
Helio Dantas <helio_dantas@outlook.com>
2019-07-14 01:36:23 -0300

    bug

--- src/migrate/migrate.js
+++ src/migrate/migrate.js
@@ -2,7 +2,7 @@ const Banck = require('../datasouce/bank');
 //const sql = ('../migrate/migrate.sql');
 
 const bank = new Banck();
-const migrate = require('./migrate.sql');
+//const migrate = require('./migrate.sql');
 const main = async () => {
    bank.isConnected();
     bank.read('alter table message add time time default \'10:00:50\';');
