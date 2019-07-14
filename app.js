a995b972de86d4efdf5c06a787ab3975fb63e872
Helio Dantas <helio_dantas@outlook.com>
2019-07-13 23:00:39 -0300

    resolvido bug quando não ha menssagem no servido e melhorado extrutura de pasta
    Signed-off-by: Helio Dantas <helio_dantas@outlook.com>

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 const helmet = require('helmet');
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 app.use(helmet());
 app.use(logger('dev'));
